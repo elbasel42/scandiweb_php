addfb9bb845f3ccd822641b2fc61ba5a9a0486f4
Abdelrahman Elbasel <abdelrahman.elbasel42@gmail.com>
2024-02-14 01:53:01 +0200

    Complete mvc

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -75,6 +75,13 @@ class Product
 
     public function read(int $id)
     {
+        $this->title = 'My first Product';
+        $this->description = 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum ';
+        $this->price = 2.56;
+        $this->sku = 'MVC-SP-PHP-01';
+        $this->image = 'https://via.placeholder.com/150';
+
+        return $this;
     }
 
     public function update(int $id, array $data)
--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,5 @@ use Symfony\Component\Routing\RouteCollection;
 
 // Routes system
 $routes = new RouteCollection();
-$routes->add('product', new Route(constant('URL_SUBFOLDER') . '/product/{id}', array('controller' => 'ProductController', 'method' => 'showAction'), array('id' => '[0-9]+')));
+$routes->add('product', new Route(URL_SUBFOLDER . '/product/{id}', ['controller' => 'ProductController', 'method' => 'showAction'], ['id' => '[0-9]+']));
+$routes->add('homepage', new Route(URL_SUBFOLDER . '/', ['controller' => 'PageController', 'method' => 'indexAction'], []));
